7b29cecbb8d6e3d7a8c9fe60ccd0ee191e5438db
tingleshao <tingleshao@gmail.com>
2016-09-30 01:00:38 -0400

    add code in simulator

--- simulator.py
+++ simulator.py
@@ -14,5 +14,21 @@ class simulator:
         # model: information about the system
         # motion: user view point
         # args: other parameters
-        time_length = len(motion)
-        total_pixel = model.h * model.w
+        if model.name == 'model0':
+            time_length = len(motion)
+            total_pixel = model.h * model.w
+
+            h_over_time = []
+            d_over_time = []
+            header_size = args.header
+            timeperiod = args.period
+            curr_t = 0
+            for i in xrange(time_length):
+                curr_t += 1
+                if curr_t == timeperiod:
+                    curr_t = 0
+                    curr_overhead += header_size
+                    curr_overhead = total_pixel - (motion[i].down_pt[0] - motion[i].start_pt[0]) * (motion[i].down_pt[1] - motion[i].start_pt[1])
+                h_over_time.append(curr_overhead)
+                d_over_time.append(0)
+        return h_over_time, d_over_time
