3a8cf0f7dc1922f90a345634992b4bc1d617ca18
Chong Shao <tingleshao@gmail.com>
2016-10-03 16:11:06 -0400

    add distribution class

---
+++ distribution.py
@@ -0,0 +1,19 @@
+# this is a class that represents a distribution
+# name: str
+# params: map
+
+
+class distribution:
+
+    def __init__(self, name, params):
+        print "you just created a distribution!"
+        self.name = name
+        self.params = params
+
+    def get_name():
+        return self.name
+
+    def get_params():
+        return self.params
+
+        
